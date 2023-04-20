4b847514205ae4113bc87cd695b2f39a8aa19173
yebun91 <65695703+yebun91@users.noreply.github.com>
2023-04-20 18:23:32 +0900

    프로젝트 생성

---
+++ i am Rich/i am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  i am Rich
+//
+//  Created by 최유진 on 2023/04/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
