5c4f6be0ec60023cc49ebfd4b0c6f957fff667b2
yebun91 <65695703+yebun91@users.noreply.github.com>
2023-04-24 21:24:59 +0900

    magic Ball 완성

---
+++ Magic-8-Ball-iOS13-master/Magic 8 Ball/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Magic 8 Ball
+//
+//  Created by Angela Yu on 14/06/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var UIImageNiew: UIImageView!
+    @IBAction func askButtonPressed(_ sender: UIButton) {
+        UIImageNiew.image = UIImage(named: "ball\(Int.random(in: 1...5))")
+    }
+}
