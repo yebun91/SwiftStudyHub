3dd4363b29fddd1033a98b28bf17948f3eed64d2
choi yujin <130713182+yudleYudle@users.noreply.github.com>
2023-04-26 15:44:45 +0900

    실로폰 완료

--- Xylophone-iOS13-master/Xylophone/ViewController.swift
+++ Xylophone-iOS13-master/Xylophone/ViewController.swift
@@ -7,15 +7,38 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
     
-
+    func playSound(_ fileName: String) {
+            let url = Bundle.main.url(forResource: fileName, withExtension: "wav")
+            player = try! AVAudioPlayer(contentsOf: url!)
+        player?.play()
+                    
+        }
+    
+    func changeOpacity(_ sender: UIButton){
+        sender.alpha = 0.5
+        print("start")
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2){
+                sender.alpha = 1
+                print("end")
+        }
+    }
+    
+    @IBAction func keyPressed(_ sender: UIButton) {
+//        if let buttonTitle = sender.currentTitle{
+//            playSound(buttonTitle)
+//        }
+        playSound(sender.currentTitle!)
+        changeOpacity(sender)
+        
+    }
 }
-
