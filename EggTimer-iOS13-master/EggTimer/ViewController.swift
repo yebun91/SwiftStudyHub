9a2d852bd090d3ad6ed9a92fb25dd15699811142
yebun91 <65695703+yebun91@users.noreply.github.com>
2023-04-27 21:42:26 +0900

    에그 타이머 딕셔너리 하는 중

--- EggTimer-iOS13-master/EggTimer/ViewController.swift
+++ EggTimer-iOS13-master/EggTimer/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
-
+    let eggTimes = ["Soft" : 5, "Medium" : 7, "Hard" : 12]
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        let hardness = sender.currentTitle!
+        print(eggTimes[hardness] ?? "Soft")
+    }
+    
 }
