61177da160ca1b666f2f7e7a5ab3038eee155db5
choi yujin <130713182+yudleYudle@users.noreply.github.com>
2023-04-28 16:59:28 +0900

    에그 타이머 초마다 텍스트 변경

--- EggTimer-iOS13-master/EggTimer/ViewController.swift
+++ EggTimer-iOS13-master/EggTimer/ViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var text: UILabel!
+    
     let eggTimes = ["Soft" : 5, "Medium" : 7, "Hard" : 12]
+    var timer = Timer()
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
-        let hardness = sender.currentTitle!
-        print(eggTimes[hardness] ?? "Soft")
+        var sec = eggTimes[sender.currentTitle!]!
+        
+        timer.invalidate()
+
+        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats:true){ timer in
+            print(sec)
+            if sec == 0 {
+                timer.invalidate()
+                self.text.text = "완료"
+            }else{
+                self.text.text = "\(sec)"
+                sec-=1
+            }
+        }
     }
     
 }
