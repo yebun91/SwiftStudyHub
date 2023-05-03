e403373a11b6fb0c1b541aeb00819dd83ef4495c
choi yujin <130713182+yudleYudle@users.noreply.github.com>
2023-05-03 11:20:53 +0900

    옵셔널 연습 끝
    타입스크립트랑 똑같네..?

--- OptionalDemo/OptionalDemo/main.swift
+++ OptionalDemo/OptionalDemo/main.swift
@@ -1,11 +1,26 @@
-//
-//  main.swift
-//  OptionalDemo
-//
-//  Created by rgorithm_mactest on 2023/05/03.
-//
+let myOptional : String?
+myOptional = nil
 
-import Foundation
+var text : String = myOptional! // error
 
-print("Hello, World!")
+if myOptional != nil {
+    text = myOptional!
+}
 
+if let safeOptional = myOptional {
+    let text2: String = safeOptional
+    text = safeOptional
+}
+
+let text2 : String = myOptional ?? "nil"
+
+struct MyOptional2 {
+    var property = 123
+    func method() {
+        print("dddd")
+    }
+}
+
+let myOptional2 : MyOptional2?
+myOptional2 = MyOptional2()
+print(myOptional2?.method())
